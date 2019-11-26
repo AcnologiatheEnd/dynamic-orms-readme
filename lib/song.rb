7e47abef2dd9382c7fe93be62d4218e9192b1e99
Winston  Chung <18.chung.winston@ctkprep.org>
2019-11-26 17:35:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,10 +26,11 @@ class Song
   end
 
   
-    def initialize(options={})
+  def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
+  end
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
